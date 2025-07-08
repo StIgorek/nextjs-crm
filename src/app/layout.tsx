a2e3e05b9b26850380db46b6bd54c5779bdd37d7
Igor <elitelifegroup@gmail.com>
2025-07-09 01:13:01 +0300

    add dashboard widgets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
 
-export const metadata = {
+/*export const metadata = {
     title: 'CRM',
     description: 'CRM program',
-};
+};*/
 
 const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
